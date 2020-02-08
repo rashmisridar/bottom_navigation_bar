d95cc35b372c3c27e2dcd3dc214a1676716dd7a8
admin <rashmisridar@gmail.com>
2020-02-08 17:58:33 +0530

    Initial commit

---
+++ lib/pages/home_fragment.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeFragment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text("Home Fragment",style: TextStyle(color: Colors.black),),
+      ),
+    );
+  }
+}
